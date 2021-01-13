cdfc6ddb436d35bfc11a05c7b5da18ced89ea1ca
Ahmed Nagy <ahmed.nagy.radwan@gmail.com>
2021-01-13 08:53:48 +0200

    finish magic Ball ;)

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,8 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    
+    @IBOutlet var ballImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
 
-
-
+    @IBAction func askButton(_ sender: UIButton) {
+        
+        ballImageView.image = ballArray.randomElement()
+    }
+    
 }
 
